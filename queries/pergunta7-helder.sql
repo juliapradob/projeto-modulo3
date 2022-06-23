bb87e80748b0934df3b19cccd4a9d7a729121fae
Julia Prado <juliapbasile@gmail.com>
2022-06-23 17:55:16 -0300

    Ajusta queries duplicadas

--- queries/pergunta7-helder.sql
+++
@@ -1,9 +0,0 @@
-use projeto_final;
-
-/*PERGUNTA 7 - Qual ano teve mais jogos lançados desde a criação da plataforma?*/
-
-SELECT 
-    appid, COUNT(appid), YEAR(release_date)
-FROM
-    steam
-GROUP BY YEAR(release_date);
