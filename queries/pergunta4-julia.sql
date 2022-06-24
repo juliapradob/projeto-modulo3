2f2dfd017c1b7e3420cf2934aeb830d7a029bffa
Julia Prado <juliapbasile@gmail.com>
2022-06-24 16:52:19 -0300

    Atualiza queries da pergunta 4

--- queries/pergunta4-julia.sql
+++ queries/pergunta4-julia.sql
@@ -1,16 +1,33 @@
--- 4. Quais jogos disponibilizam e-mail de suporte? Esses jogos possuem mais avaliações positivas ou negativas?
+-- 4.a) Os jogos que disponibilizam e-mail de suporte possuem mais avaliações positivas ou negativas?
 
 SELECT 
     steam_support_info.appid,
+    steam_support_info.support_email,
     steam.name,
     steam.positive_ratings,
     steam.negative_ratings,
-    GREATEST(steam.positive_ratings,
-            steam.negative_ratings)
+    steam.positive_ratings - steam.negative_ratings AS diferença
 FROM
     steam_support_info
         INNER JOIN
     steam ON steam.appid = steam_support_info.appid
 WHERE
     steam_support_info.support_email IS NOT NULL
-ORDER BY GREATEST(steam.positive_ratings, steam.negative_ratings) DESC;
+ORDER BY diferença DESC; 
+
+-- 4.b) Os jogos que não disponibilizam e-mail de suporte possuem mais avaliações positivas ou negativas?
+
+SELECT 
+    steam_support_info.appid,
+    steam_support_info.support_email,
+    steam.name,
+    steam.positive_ratings,
+    steam.negative_ratings,
+    steam.positive_ratings - steam.negative_ratings AS diferença
+FROM
+    steam_support_info
+        INNER JOIN
+    steam ON steam.appid = steam_support_info.appid
+WHERE
+    steam_support_info.support_email IS NULL
+ORDER BY diferença DESC; 
